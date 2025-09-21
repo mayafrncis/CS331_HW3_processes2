ebbb19e68b81af7a219e34f10a3ed93b527dc738
mayafrncis <maya.ara.francis@gmail.com>
2025-09-21 14:53:00 +0000

    q3

---
+++ assign3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void function1() {
+	printf("Function 1 is running.\n");
+}
+
+void function2() {
+	printf("Function 2 is running.\n");
+}
+
+int main() {
+
+	printf("Program is running.\n");
+	atexit(function2);
+        atexit(function1);
+	printf("End of program\n");
+	return 0;
+}
