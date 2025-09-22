87e7a602fe1c9a55a9189f9ac6a287f823f39f09
mayafrncis <maya.ara.francis@gmail.com>
2025-09-22 09:34:12 +0000

    edited

--- assign2.c
+++ assign2.c
@@ -3,21 +3,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 int main() {
-        int ret = fork();
-        if (ret == 0) {
-                printf("Child PID: %d\n", getpid());
-                exit(0);
-        }
+	int ret = fork();
+	if (ret == 0) {
+		printf("Child PID: %d\n", getpid());
+		exit(0);
+	}
 	int ret2 = fork();
-	if (ret2 == 0)
+	if (ret2 == 0) {
 		printf("2nd Child PID: %d\n", getpid());
-	else {
+		exit(0);
+	} else {
 		int exitStatus_child2, exitStatus_child1;
-		waitpid(ret2, &exitStatus_child2,0); //waited for child2 first, in case it waits on child 2 when I use wait(NULL instead of 1, which would mean child 1 wouldnt be waited for
+		waitpid(ret2, &exitStatus_child2,0);
 		wait(&exitStatus_child1);
-                printf("Parent PID: %d\n", getpid());
-		printf("Exit status of child 1: %d\n", exitStatus_child1);
-		printf("Exit status of child 2: %d\n", exitStatus_child2);
-        }
+		printf("Parent PID: %d\n", getpid());
+		if (WIFEXITED(exitStatus_child1) & WIFEXITED(exitStatus_child2)) {
+			printf("Exit status of child 1: %d\n", WEXITSTATUS(exitStatus_child1));
+			printf("Exit status of child 2: %d\n", WEXITSTATUS(exitStatus_child2));
+        	}
+	}
 	return 0;
 }
